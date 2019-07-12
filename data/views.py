572211ad32ef8ab4c6fd0f1ddc0748d67fa17337
rgarcia <r.gc@hotmail.es>
2019-07-12 12:24:52 +0200

    - Add new dataprice_by_subcategory view.

--- data/views.py
+++ data/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User, Group
-from rest_framework import viewsets
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from .models import Category, DataPrice, SubCategory
 from .serializers import UserSerializer, GroupSerializer, CategorySerializer, SubCategorySerializer, DataPriceSerializer
@@ -12,6 +15,21 @@ class CategoryViewSet(viewsets.ModelViewSet):
 class SubCategoryViewSet(viewsets.ModelViewSet):
     queryset = SubCategory.objects.all()
     serializer_class = SubCategorySerializer
+
+    @action(detail=True, methods=['get'])
+    def dataprice_by_subcategory(self, request, pk=None):
+        """ Return DataPrices to generate Charts in Front-End """
+        subcategory = get_object_or_404(SubCategory, pk=pk)
+
+        return Response(
+            [
+                {'name': subcategory.name,
+                 'price': dataprice.price, 
+                 'date': dataprice.date
+                } for dataprice in DataPrice.objects.filter(subcategory=subcategory).order_by('date')
+            ],
+        status=status.HTTP_200_OK)
+
 
 class DataPriceViewSet(viewsets.ModelViewSet):
     queryset = DataPrice.objects.all()
