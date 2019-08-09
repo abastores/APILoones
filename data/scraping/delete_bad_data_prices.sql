c6eddf88da8ce6209501e0c6156be1e1a4b22824
rgarcia <r.gc@hotmail.es>
2019-08-09 18:21:58 +0200

    New SQL data.

---
+++ data/scraping/delete_bad_data_prices.sql
@@ -0,0 +1,23 @@
+-- VIRGIN OLIVE OIL
+DELETE FROM loones.data_dataprice WHERE date = '2018-1-1 00:00:00.000000' AND subcategory_id = 2;
+DELETE FROM loones.data_dataprice WHERE date = '2018-1-8 00:00:00.000000' AND subcategory_id = 2;
+
+-- LAMPANTE OLIVE OIL
+DELETE FROM loones.data_dataprice WHERE date = '2018-1-1 00:00:00.000000' AND subcategory_id = 3;
+DELETE FROM loones.data_dataprice WHERE date = '2018-1-8 00:00:00.000000' AND subcategory_id = 3;
+
+-- RED WINE
+DELETE FROM loones.data_dataprice WHERE date = '2018-1-1 00:00:00.000000' AND subcategory_id = 4;
+
+-- WHITE WINE
+DELETE FROM loones.data_dataprice WHERE date = '2018-1-1 00:00:00.000000' AND subcategory_id = 5;
+
+-- SHELL RICE
+DELETE FROM loones.data_dataprice WHERE date = '2018-1-1 00:00:00.000000' AND subcategory_id = 6;
+DELETE FROM loones.data_dataprice WHERE date = '2018-1-8 00:00:00.000000' AND subcategory_id = 6;
+
+-- BARLEY FEED
+DELETE FROM loones.data_dataprice WHERE date = '2018-1-1 00:00:00.000000' AND subcategory_id = 7;
+
+-- SOFT BREAD WHEAT
+DELETE FROM loones.data_dataprice WHERE date = '2018-1-1 00:00:00.000000' AND subcategory_id = 8;
