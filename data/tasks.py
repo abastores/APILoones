82a1a2b1bfead324fc9df741e5f2f0ddbdae0cc2
rgarcia <r.gc@hotmail.es>
2019-08-21 20:30:07 +0200

    Fix: - Fixed not found directory or file in Mapgob periodically scraping.

--- data/tasks.py
+++ data/tasks.py
@@ -1,10 +1,13 @@
 from django.core.management import call_command
 from django.db import connection
 from celery import task
+import os
+
+module_dir = os.path.dirname(__file__)  # get current directory
 
 POOLRED_COMMAND_NAME = 'poolred_scraping'
 MAPGOB_COMMAND_NAME = 'mapgob_periodic_scraping'
-FILE_PATH = 'scraping/delete_bad_data_prices.sql'
+FILE_PATH = os.path.join(module_dir, 'scraping/delete_bad_data_prices.sql')
 
 @task(name='poolred_scraping_data') 
 def poolred_scraping_data():
